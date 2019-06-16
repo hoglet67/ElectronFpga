d0dbaced1f6b260ba9d3bc149e0d6107f93b5303
Phillip Pearson <philpearson@google.com>
2019-06-16 06:17:00 -0700

    firmware_d11 now builds and uploads using arduino-cli with 'make clean upload'

--- firmware_d11/README.md
+++ firmware_d11/README.md
@@ -4,23 +4,13 @@ max10_electron_ula board.
 It requires the MattairTech fork of ArduinoCore-samd to be installed;
 see https://github.com/mattairtech/ArduinoCore-samd for instructions.
 
-Bootloader programming
-----------------------
+The Makefile needs arduino-cli to be installed, as well as the J-Link command
+line tools.
 
-The bootloader I'm using is sam_ba_Generic_D11C14A_SAMD11C14A.bin, from [this
-archive](https://www.mattairtech.com/software/arduino/SAM-BA-bootloaders-zero-mattairtech.zip),
-programmed using my J-Link like this:
+Right now the code doesn't even fit in the 12kB left over once the bootloader
+is added, so I have to build without the bootloader and program using the
+J-Link.
 
 ~~~~
-JLinkExe -device atsamd11c14 -if swd -speed 4000
-connect
-erase
-loadbin sam_ba_Generic_D11C14A_SAMD11C14A.bin, 0
-r
-go
-q
+make clean upload; sleep 3; screen /dev/tty.usbmodem*; reset
 ~~~~
-
-After this, the board enumerates as a MattairTech LLC Generic SAMD11C14A, and I
-can program it using the Arduino IDE, using the CDC_HID, 4KB_BOOTLOADER,
-INTERNAL_USB_CALIBRATED_OSCILLATOR, and NO_UART_ONE_WIRE_ONE_SPI options.
--- firmware_d11/firmware_d11.ino
+++ firmware_d11/firmware_d11.ino
@@ -6,13 +6,85 @@
  * the flash.
  */
 
+#include <SPI.h>
+
+// For pinPeripheral(), so we can change PINMUX
+#include "wiring_private.h"
+
+/*
+ * FPGA serial port
+ *
+ * TXD = PA14 sercom2.0
+ * RXD = PA15 sercom2.1
+ */
+#define TXD_PIN 14
+#define RXD_PIN 15
+
+/*
+ * FPGA SPI port:
+ *
+ * MISO = PA09 sercom0.3 (alt)
+ * SCK  = PA05 sercom0.1 (alt)
+ * MOSI = PA04 sercom0.0 (alt)
+ * SS   = PA08 sercom0.2 (alt)
+ */
+
+#define FPGA_MISO_PIN 9
+#define FPGA_SCK_PIN  5
+#define FPGA_MOSI_PIN 4
+#define FPGA_SS_PIN   8
+
+SPIClass fpga_spi(&sercom0, FPGA_MISO_PIN, FPGA_SCK_PIN, FPGA_MOSI_PIN, SPI_PAD_0_SCK_1, SERCOM_RX_PAD_3);
+
 void setup() {
   Serial.begin(9600);
+
+  pinMode(FPGA_SS_PIN, OUTPUT);
+  digitalWrite(FPGA_SS_PIN, HIGH);
+  fpga_spi.begin();
+  // All three of these need to be on SERCOM_ALT; I think this is automatic here.
+  // The following three lines may be unnecessary.
+  pinPeripheral(FPGA_MOSI_PIN, PIO_SERCOM);
+  pinPeripheral(FPGA_SCK_PIN,  PIO_SERCOM);
+  pinPeripheral(FPGA_MISO_PIN, PIO_SERCOM);
+  // 1MHz for debugging so I can easily catch it on a logic analyzer
+  fpga_spi.beginTransaction(SPISettings(1000000L, MSBFIRST, SPI_MODE0));
+
+  // Right now TXD_PIN switches the SPI port between the flash (low) and FPGA (high)
+  pinMode(TXD_PIN, OUTPUT);
+  digitalWrite(TXD_PIN, HIGH);
 }
 
 void loop() {
   if (Serial.dtr()) {
-    Serial.print("hello!\n");
+    digitalWrite(TXD_PIN, HIGH);
+    digitalWrite(FPGA_SS_PIN, LOW);
+    Serial.print("FPGA: ");
+    Serial.print(fpga_spi.transfer(0), HEX);
+    Serial.print(" ");
+    Serial.print(fpga_spi.transfer(0), HEX);
+    Serial.print(" ");
+    Serial.print(fpga_spi.transfer(0), HEX);
+    Serial.print(" ");
+    Serial.println(fpga_spi.transfer(0), HEX);
+    digitalWrite(FPGA_SS_PIN, HIGH);
+
+    digitalWrite(TXD_PIN, LOW);
+    digitalWrite(FPGA_SS_PIN, LOW);
+    Serial.print("Flash: ");
+    Serial.print(fpga_spi.transfer(0x90), HEX);
+    Serial.print(" ");
+    Serial.print(fpga_spi.transfer(0), HEX);
+    Serial.print(" ");
+    Serial.print(fpga_spi.transfer(0), HEX);
+    Serial.print(" ");
+    Serial.print(fpga_spi.transfer(0), HEX);
+    Serial.print(" ");
+    Serial.print(fpga_spi.transfer(0), HEX);
+    Serial.print(" ");
+    Serial.println(fpga_spi.transfer(0), HEX);
+    digitalWrite(FPGA_SS_PIN, HIGH);
+
     delay(1000);
   }
 }
