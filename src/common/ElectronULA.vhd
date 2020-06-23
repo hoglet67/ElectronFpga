hoglet67/ElectronFpga

commit 77489cf6f20df1e74e68069f0705ed0bb80208e1

ULA: Fix a bug where some ROMs were accessed at 1MHz rather than 2MHz
Change-Id: Ib8cd30d77d2fe2497c3214675a8ee70a0b64080c

src/common/ElectronULA.vhd
@@ -258,6 +258,7 @@ architecture behavioral of ElectronULA is
   signal contention     : std_logic;
   signal contention1    : std_logic;
   signal contention2    : std_logic;
+  signal io_access      : std_logic; -- always at 1MHz, no contention
   signal rom_access     : std_logic; -- always at 2mhz, no contention
   signal ram_access     : std_logic; -- 1MHz/2Mhz/Stopped
 
@@ -466,7 +467,7 @@ begin
     -- The external ROM is enabled:
     -- - When the address is C000-FBFF and FF00-FFFF (i.e. OS Rom)
     -- - When the address is 8000-BFFF and the ROM 10 or 11 is paged in (101x)
-    ROM_n_int <= '0' when addr(15 downto 14) = "11" and addr(15 downto 8) /= x"FC" and addr(15 downto 8) /= x"FD" and addr(15 downto 8) /= x"FE" else
+    ROM_n_int <= '0' when addr(15 downto 14) = "11" and io_access = '0' else
                  '0' when addr(15 downto 14) = "10" and page_enable = '1' and page(2 downto 1) = "01" else
                  '1';
 
@@ -1122,11 +1123,14 @@ begin
 -- clock enable generator
 --------------------------------------------------------
 
-    -- ROM accesses always happen at 2Mhz
-    rom_access <= not ROM_n_int;
-    -- RAM accesses always happen at 1Mhz and subber contention
+    -- IO accesses always happen at 1MHz (no contention)
+    io_access <= '1' when addr(15 downto 8) = x"FC" or addr(15 downto 8) = x"FD" or addr(15 downto 8) = x"FE" else '0';
+
+    -- ROM accesses always happen at 2MHz (no contention)
+    rom_access <= addr(15) and not io_access;
+
+    -- RAM accesses always happen at 1MHz (with contention)
     ram_access <= not addr(15);
-    -- IO accesses always happen at 1MHz and don't suffer contention
 
     clk_gen1 : process(clk_16M00, RST_n)
     begin

commit 65a578ab8a8b183b329063a0363b3713f8c0d4f1

ULA: fix regression in keyboard timings introduced in 77489cf6

src/common/ElectronULA.vhd
@@ -1124,7 +1124,8 @@ begin
 --------------------------------------------------------
 
     -- IO accesses always happen at 1MHz (no contention)
-    io_access <= '1' when addr(15 downto 8) = x"FC" or addr(15 downto 8) = x"FD" or addr(15 downto 8) = x"FE" else '0';
+    -- This includes keyboard reads in paged ROM slots 8/9
+    io_access <= '1' when addr(15 downto 8) = x"FC" or addr(15 downto 8) = x"FD" or addr(15 downto 8) = x"FE" or (addr(15 downto 14) = "10" and page_enable = '1' and page(2 downto 1) = "00") else '0';
 
     -- ROM accesses always happen at 2MHz (no contention)
     rom_access <= addr(15) and not io_access;