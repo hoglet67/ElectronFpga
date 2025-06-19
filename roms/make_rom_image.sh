dbfe1e14c1182e53a24a95bd3c4f14157304d5a3
David Banks <dave@hoglet.com>
2025-06-19 18:37:18 +0100

    ROMS: Update AP6 ROM to latest with Tube support

--- roms/make_rom_image.sh
+++ roms/make_rom_image.sh
@@ -31,7 +31,7 @@ cat Basic2.rom                 >> $IMAGE
 cat Basic2.rom                 >> $IMAGE
 
 # Slots C-F
-cat pres_ap2_v1_23.rom         >> $IMAGE
+cat AP6v134t.rom               >> $IMAGE
 cat blank.rom                  >> $IMAGE
 cat blank.rom                  >> $IMAGE
 cat M7_191.rom                 >> $IMAGE
