920397713c2d52ac92a35341979d219e9f6324d5
David Banks <dave@hoglet.com>
2025-08-16 12:06:18 +0100

    ROMs: Updates needed by last commit

--- roms/make_rom_image.sh
+++ roms/make_rom_image.sh
@@ -12,21 +12,22 @@ echo Making $IMAGE
 
 rm -f $IMAGE
 
-# Slots 0-3 (sideways RAM)
-cat blank.rom                  >> $IMAGE
-cat blank.rom                  >> $IMAGE
-cat blank.rom                  >> $IMAGE
-cat blank.rom                  >> $IMAGE
+# Slots 0-3 (ABR cartridge sideways RAM) so cannot be preloaded
+# So we borrow these for the various OS images and Stop Press 64 in Slot A
+cat os100.rom                  >> $IMAGE
+cat 524-OS-3.00-alt4.rom       >> $IMAGE
+cat sp64_lower.rom             >> $IMAGE
+cat sp64_upper.rom             >> $IMAGE
 
-# Slots 4-7 (sideways RAM)
+# Slots 4-7
 cat mmfs_swram.rom             >> $IMAGE
 cat blank.rom                  >> $IMAGE
 cat blank.rom                  >> $IMAGE
 cat blank.rom                  >> $IMAGE
 
 # Slots 8-B
-cat os100.rom                  >> $IMAGE
-cat os100.rom                  >> $IMAGE
+cat blank.rom                  >> $IMAGE
+cat blank.rom                  >> $IMAGE
 cat Basic2.rom                 >> $IMAGE
 cat Basic2.rom                 >> $IMAGE
 
